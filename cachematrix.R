aa648c81853b2d7c76ab3ca9162efc9d2cf0452c
searchant <zhaogz@yahoo.com>
2016-01-19 20:31:31 -0500

    Matrix inverse
    2nd submission

--- cachematrix.R
+++ cachematrix.R
@@ -1,4 +1,4 @@
-
+# 1/19/2016
 ## makeCacheMatrix creates a special "matrix" object that can cache its inverse
 ## cacheSolve computes the inverse of the special "matrix" returned by makeCacheMatrix
 # If the inverse has already been calculated (and the matrix has not changed), then the cachesolve should retrieve the inverse from the cache
